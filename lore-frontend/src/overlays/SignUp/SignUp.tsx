1a9cd31532be462d9a55f5130181cfb14b7f046f
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2025-03-17 21:20:47 +0000

    front: Axios, sign up

--- lore-frontend/src/overlays/SignUp/SignUp.tsx
+++ lore-frontend/src/overlays/SignUp/SignUp.tsx
@@ -1,13 +1,28 @@
 import { OverlayType } from "../../constants/types.ts";
 import icon_cross from "../../assets/icon-cross-rotated.svg";
-import { useContext, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { OverlayContext } from "../../constants/contexts.ts";
 import LabeledInput from "../../components/LabeledInput/LabeledInput.tsx";
+import api from "../../utils/api.ts";
 
 function SignUp() {
     const [, setOverlayType] = useContext(OverlayContext)!;
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        api.post("users", {
+            username: username,
+            password: password
+        })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
 
     return (
         <div className="w-[500px] pt-1 px-1 pb-3 bg-dark rounded-2xl">
@@ -22,24 +37,31 @@ function SignUp() {
             </div>
             <div className="px-3 flex flex-col grow">
                 <h1 className="text-center text-2xl font-[700] text-white-custom">Sign Up</h1>
-                {/* Labeled Inputs */}
-                <LabeledInput label="Username" value={username} setValue={setUsername}/>
-                <LabeledInput label="Password" value={password} setValue={setPassword}/>
-                <div className="mt-1 text-sm text-blue-light-custom-3">
-                    Already have an account?
+                <form onSubmit={handleSubmit}>
+                    {/* Labeled Inputs */}
+                    <LabeledInput label="Username" value={username} setValue={setUsername}/>
+                    <LabeledInput label="Password" value={password} setValue={setPassword}/>
+
+                    {/* text prompt for Log In */}
+                    <div className="mt-1 text-sm text-blue-light-custom-3">
+                        Already have an account?
+                        <button
+                            type="button"
+                            onClick={() => setOverlayType(OverlayType.LogIn)}
+                            className="ml-0.25 text-blue-500"
+                        >
+                            Log In
+                        </button>
+                    </div>
+
+                    {/* Sign Up Button */}
                     <button
-                        onClick={() => setOverlayType(OverlayType.LogIn)}
-                        className="ml-0.25 text-blue-500"
+                        type="submit"
+                        className="mt-2.5 w-full py-0.5 px-1 text-dark font-bold bg-white-custom rounded-2xl"
                     >
-                        Log In
+                        Sign Up
                     </button>
-                </div>
-                {/* Log In Button */}
-                <button
-                    className="mt-2.5 py-0.5 px-1 text-dark font-bold bg-white-custom rounded-2xl"
-                >
-                    Sign Up
-                </button>
+                </form>
             </div>
         </div>
     );
