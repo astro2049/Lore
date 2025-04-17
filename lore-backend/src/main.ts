ea4a39b8ae79506a25fd16e8035d64d6b2ef9d78
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2025-04-18 00:55:52 +0100

    nest / mini update: cors config

--- lore-backend/src/main.ts
+++ lore-backend/src/main.ts
@@ -3,15 +3,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
+import * as process from "node:process";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
-    app.enableCors({
-        origin: "http://localhost:5173",
-        credentials: true
-    });
+    // Cors
+    if (process.env.NODE_ENV !== "production") {
+        app.enableCors({
+            origin: "http://localhost:5173",
+            credentials: true
+        });
+    }
     await app.listen(process.env.PORT ?? 3000);
 }
 
